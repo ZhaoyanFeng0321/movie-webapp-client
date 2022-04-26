36f770da8f7c516b58c901db37ed58b55d3d7783
Zhaoyan <irisfeng0321@outlook.com>
2022-04-25 22:17:16 -0400

    fix edit profile

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import './vendors/bootstrap/bootstrap.min.css';
 import './vendors/fontawesome/css/all.min.css';
 import './utils/index.css';
-import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import {HashRouter, Link, Route, Routes} from "react-router-dom";
 import Signup from "./screens/signup";
 import Login from "./screens/login";
 import Profile from "./screens/profile";
@@ -10,17 +10,19 @@ import EditProfile from "./screens/profile/edit-profile";
 import HomeScreen from "./home/HomeScreen/HomeScreen";
 import Search from "./home/Search/Search";
 import ResultScreen from "./home/Search/ResultScreen";
+import {Provider} from "react-redux";
 
 function App() {
   return (
-    // <ProfileProvider>
-    <BrowserRouter>
+    <HashRouter>
       <div className="container">
         <Routes>
           {/*<Route path="/">*/}
+
             <Route path="/profile" element={<Profile/>}/>
             <Route path="/profile/:username/edit" element={<EditProfile/>}/>
             <Route path="/profile/:username" element={<Profile/>}/>
+            <Route path="/profile/:username/*" element={<Profile />}/>
             <Route path="/">
                 <Route index element={<HomeScreen/>} />
                 <Route path="home" exact={true} element={<HomeScreen/>}/>
@@ -33,8 +35,7 @@ function App() {
           {/*</Route>*/}
         </Routes>
       </div>
-    </BrowserRouter>
-    // </ProfileProvider>
+    </HashRouter>
   );
 }
 
