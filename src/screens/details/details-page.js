91772299754e40a9bd63a9680370a0868b5240f2
Jiayi0928 <mmajiayi@163.com>
2022-04-28 00:26:20 -0400

    fix navigation issue at detail page

--- src/screens/details/details-page.js
+++ src/screens/details/details-page.js
@@ -4,25 +4,35 @@ import axios from "axios";
 //import Preformatted from "../../../movie-detail/src/components/preformatted";
 // import SecureContent from "../components/secure-content";
 import Navigation from "../../home/Navigation/Navigation";
+import * as authService from "../../services/auth-service";
+import NavigationPersonal from "../../home/Navigation/NavigationPersonal";
 
 const OmdbDetails = () => {
     const [movieDetails, setMovieDetails] = useState({})
     // const [ourMovieDetails, setOurMovieDetails] = useState({})
     const url = 'https://www.omdbapi.com/?apikey=b2bd5979'
     const {imdbID} = useParams()
-    const searchMovieByImdbID = async () => {
-        const response = await axios.get(`${url}&i=${imdbID}`)
-        setMovieDetails(response.data)
-    }
+    // const searchMovieByImdbID = async () => {
+    //     const response = await axios.get(`${url}&i=${imdbID}`)
+    //     setMovieDetails(response.data)
+    // }
     // const searchOurMovieByImdbID = async () => {
     //     const response = await axios.get(`http://localhost:4000/api/movies/${imdbID}`)
     //     setOurMovieDetails(response.data)
     // }
 
-    useEffect(() => {
-        searchMovieByImdbID()
-        // searchOurMovieByImdbID()
-    }, [])
+    useEffect(()=> {
+        async function searchMovieByImdbID () {
+            const response = await axios.get(`${url}&i=${imdbID}`)
+            setMovieDetails(response.data)
+        }
+        searchMovieByImdbID();
+    },[])
+
+    // useEffect(() => {
+    //     searchMovieByImdbID()
+    //     // searchOurMovieByImdbID()
+    // }, [])
 
     // const handleLike = async () => {
     //     const movie = {
@@ -33,14 +43,45 @@ const OmdbDetails = () => {
     //     const response = await axios.post("http://localhost:4000/api/likes", movie)
     //     setOurMovieDetails(response.data)
     // }
+    const {username} = useParams();
+    const [profile, setProfile] = useState({});
+    const [currentUser,setCurrentUser] = useState({});
+
+    useEffect(async () => {
+        try {
+            let user = await authService.profile();
+            setCurrentUser(user);
+            if(username!==user.username){
+                user = await authService.findUser(username);
+            }else{
+                user = await authService.findUser(username);
+                setCurrentUser(user);
+            }
+            setProfile(user);
+        } catch (e) {
+
+        }
+    }, [username]);
 
     return (
         <div className="text-warning">
 
-            <div className="row mt-3 ms-5 me-5">
-                <Navigation/>
+            {currentUser.accountType !== "PERSONAL"  && currentUser.accountType !== "ACTOR"  &&currentUser.accountType !== "ADMIN"  &&
+                <div className="row mt-3 ms-5 me-5">
+                    <Navigation/>
 
-            </div>
+                </div>}
+
+            {(currentUser.accountType === "PERSONAL"|| currentUser.accountType === "ACTOR" || currentUser.accountType === "ADMIN")  &&
+                <div className="row mt-3 ms-5 me-5">
+                    <NavigationPersonal/>
+
+                </div>}
+
+            {/*<div className="row mt-3 ms-5 me-5">*/}
+            {/*    <Navigation/>*/}
+
+            {/*</div>*/}
 
             <div>
             <div className="row ms-5 me-5">
@@ -51,7 +92,7 @@ const OmdbDetails = () => {
 
             <div className="row ms-5 me-5">
                 <div className="col-3">
-                    <img src={movieDetails.Poster} height={300}/>
+                    <img src={movieDetails.Poster} height={300} alt=""/>
                     <p className="mt-3"><i className="fa-solid fa-star-sharp"></i> {movieDetails.imdbRating}</p>
                     <button className="btn btn-warning"><i className="fa-solid fa-thumbs-up"></i> </button>
                     <button className="btn btn-warning ms-3"><i className="fa-solid fa-plus"></i> </button>
