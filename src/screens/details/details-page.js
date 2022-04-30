a27390cd6b2b3cb2aa8093f2f5d3c7d78d40d9c8
eddieyan95 <yufanyan95@gmail.com>
2022-04-30 14:55:25 -0400

    add review

--- src/screens/details/details-page.js
+++ src/screens/details/details-page.js
@@ -8,32 +8,51 @@ import {Link} from "react-router-dom";
 import Navigation from "../../home/Navigation/Navigation";
 import * as authService from "../../services/auth-service";
 import NavigationPersonal from "../../home/Navigation/NavigationPersonal";
+import {addMovieToList} from "../../services/auth-service";
 import * as service from "../../services/review-service";
 import ReviewItem from "./reviewItem";
+import PostReview from "./post-review-component";
+
 
 const OmdbDetails = () => {
     const [movieDetails, setMovieDetails] = useState([])
+    // const [ourMovieDetails, setOurMovieDetails] = useState({})
     const url = 'https://www.omdbapi.com/?apikey=b2bd5979'
     const {imdbID} = useParams()
 
     const [login, setLogin] = useState(false);
-    const [currentUser, setCurrentUser] = useState("");
-
-    const searchMovieByImdbID = async (imdbID) => {
+    const [currentUser,setCurrentUser] = useState({});
+    // const searchMovieByImdbID = async () => {
+    //     const response = await axios.get(`${url}&i=${imdbID}`)
+    //     setMovieDetails(response.data)
+    // }
+    // const searchOurMovieByImdbID = async () => {
+    //     const response = await axios.get(`http://localhost:4000/api/movies/${imdbID}`)
+    //     setOurMovieDetails(response.data)
+    // }
+    const searchMovieByImdbID = async (imdbID)=> {
         const response = await axios.get(`${url}&i=${imdbID}`)
         setMovieDetails(response.data)
     }
-    useEffect(async () => {
+    useEffect(async ()=> {
         await searchMovieByImdbID(imdbID);
         try {
             let user = await authService.profile();
-            await setCurrentUser(user);
+            setCurrentUser(user);
             setLogin(true);
         } catch (e) {
 
         }
+        // if(username!==user.username){
+        //     user = await authService.findUser(username);
+        // }else{
+        //     user = await authService.findUser(username);
+        //     setCurrentUser(user);
+        // }
+        // setProfile(user);
 
-    }, [])
+
+    },[])
 
 
 
@@ -48,62 +67,111 @@ const OmdbDetails = () => {
         }catch (e) {
         }
 
-    }, [])
+    },[])
 
-    const addMovieToList = async (name, movieID) => {
-        await authService.addMovieToList(name, movieID).catch(e => alert(e));
-    }
+
+
+    // useEffect(() => {
+    //     searchMovieByImdbID()
+    //     // searchOurMovieByImdbID()
+    // }, [])
+
+    // const handleLike = async () => {
+    //     const movie = {
+    //         title: movieDetails.Title,
+    //         poster: movieDetails.Poster,
+    //         imdbID: movieDetails.imdbID
+    //     }
+    //     const response = await axios.post("http://localhost:4000/api/likes", movie)
+    //     setOurMovieDetails(response.data)
+    // }
+
+    const addMovieToList = async (uid, movieID) =>
+
+            await authService.addMovieToList(uid, movieID);
+
+
 
     return (
         <div className="text-warning">
 
-            <div>
-                <div className="row ms-5 me-5">
+{/*<<<<<<< HEAD*/}
+{/*            <div className="row mt-3 ms-5 me-5">*/}
+{/*                /!*<Navigation/>*!/*/}
+{/*            </div>*/}
+{/*=======*/}
+{/*            {currentUser.accountType !== "PERSONAL"  && currentUser.accountType !== "ACTOR"  &&currentUser.accountType !== "ADMIN"  &&*/}
+            {!login &&
+                <div className="row mt-3 ms-5 me-5">
+                    <Navigation/>
 
-                    <h1 className="fw-bold wd-gold">{movieDetails.Title}</h1>
+                </div>}
+
+            {/*{(currentUser.accountType === "PERSONAL"|| currentUser.accountType === "ACTOR" || currentUser.accountType === "ADMIN")  &&*/}
+            {login &&
+                <div className="row mt-3 ms-5 me-5">
+                    <NavigationPersonal/>
+
+                </div>}
+
+            {/*<div className="row mt-3 ms-5 me-5">*/}
+            {/*    <Navigation/>*/}
+
+            {/*</div>*/}
+{/*>>>>>>> 91772299754e40a9bd63a9680370a0868b5240f2*/}
+
+            {/*<div className="row ms-5 me-5">*/}
+            {/*    <Link to="/result/:movieSearch">*/}
+            {/*        <i className="fa-solid fa-circle-chevron-left"></i>*/}
+            {/*    </Link>*/}
+            {/*</div>*/}
+
+            <div>
+            <div className="row ms-5 me-5">
+
+                <h1 className="fw-bold wd-gold">{movieDetails.Title}</h1>
+            </div>
+
+
+            <div className="row ms-5 me-5">
+                <div className="col-3">
+                    <img src={movieDetails.Poster} height={300} alt=""/>
+                    <p className="mt-3"><i className="fa-solid fa-star-sharp"></i> {movieDetails.imdbRating}</p>
+                    {/*<button className="btn btn-warning"><i className="fa-solid fa-thumbs-up"></i> </button>*/}
+                    <button onClick={()=>addMovieToList(currentUser._id, imdbID)} className="btn btn-block btn-warning" ><i className="fa-solid fa-plus"></i> Add to List</button>
                 </div>
 
 
-                <div className="row ms-5 me-5">
-                    <div className="col-3">
-                        <img src={movieDetails.Poster} height={300} alt=""/>
-                        <p className="mt-3"><i
-                            className="fa-solid fa-star-sharp"></i> {movieDetails.imdbRating}</p>
-                        {/*<button className="btn btn-warning"><i className="fa-solid fa-thumbs-up"></i> </button>*/}
-                        <button onClick={() => addMovieToList(currentUser.username, imdbID)}
-                                className="btn btn-block btn-warning"><i
-                            className="fa-solid fa-plus"></i> Add to Watch
-                        </button>
-                    </div>
+                <div className="col-9 d-none d-md-block">
+                    <p>{movieDetails.Plot}</p>
+                    <p> Year Released: {movieDetails.Year}</p>
+                    <p> Rated: {movieDetails.Rated}</p>
+                    <p> Runtime : {movieDetails.Runtime}</p>
+                    <p> Genre: {movieDetails.Genre}</p>
+                    <p> Director: {movieDetails.Director}</p>
+                    <p> Actors: {movieDetails.Actors}</p>
+                    <p> Language: {movieDetails.Language}</p>
+                    <p> Country: {movieDetails.Country}</p>
+                    <p> Award: {movieDetails.Awards}</p>
+                    <p> Box Office :{movieDetails.BoxOffice}</p>
+                    <h2>Post your review</h2>
+                    <PostReview mid={imdbID} findReviewsByOMDB={findReviewsByOMDB}/>
 
 
-                    <div className="col-9 d-none d-md-block">
-                        <p>{movieDetails.Plot}</p>
-                        <p> Year Released: {movieDetails.Year}</p>
-                        <p> Rated: {movieDetails.Rated}</p>
-                        <p> Runtime : {movieDetails.Runtime}</p>
-                        <p> Genre: {movieDetails.Genre}</p>
-                        <p> Director: {movieDetails.Director}</p>
-                        <p> Actors: {movieDetails.Actors}</p>
-                        <p> Language: {movieDetails.Language}</p>
-                        <p> Country: {movieDetails.Country}</p>
-                        <p> Award: {movieDetails.Awards}</p>
-                        <p> Box Office :{movieDetails.BoxOffice}</p>
-                        {/*<SecureContent>*/}
-                        {/*    <button onClick={handleLike}>Like ({ourMovieDetails && ourMovieDetails.likes})</button>*/}
-                        {/*</SecureContent>*/}
+
+
+
+                    {/*<SecureContent>*/}
+                    {/*    <button onClick={handleLike}>Like ({ourMovieDetails && ourMovieDetails.likes})</button>*/}
+                    {/*</SecureContent>*/}
 
                     {/*<Preformatted obj={movieDetails}/>*/}
                 </div>
             </div>
 
 
-
                 <div className="row ms-5 me-5 mt-5">
                     <h3 className="fw-bold wd-gold">User reviews</h3>
-                    {reviews.length === 0 &&
-                        <h5 className="wd-white">No reviews for this movie. Be the first!</h5>
-                    }
                 <ul className="list-group">
                     {
                         reviews && reviews.map(review =>
@@ -113,9 +181,8 @@ const OmdbDetails = () => {
                 </ul>
                 </div>
 
-                        {/*<Preformatted obj={movieDetails}/>*/}
-                    </div>
-                </div>
+            </div>
+        </div>
 
 
     );
